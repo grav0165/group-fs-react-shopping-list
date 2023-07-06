6df97e06615c91c64a2d86468799c526d8b7bb83
Aubrey Graves <grav0165@gmail.com>
2023-07-06 12:04:47 -0500

    Created database.sql file

--- database.sql
+++ database.sql
@@ -1,2 +1,14 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+-- Table Structure
+CREATE TABLE shoppingList (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(80) NOT NULL,
+    quantity NUMERIC NOT NULL,
+    unit VARCHAR(20),
+    purchased BOOLEAN DEFAULT FALSE
+);
+
+INSERT INTO shoppingList (name, quantity, unit)
+VALUES ('Apples', '5', 'lbs'), ('Bread', '1', 'loaf'), ('Milk', '1', 'gallon'), ('Sliced Almonds', '2', 'cups'), ('Bananas', '1', 'bunch');
