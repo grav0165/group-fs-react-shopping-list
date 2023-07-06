0fbae5728a15b1365161f12ad6996b449245f0c5
Quynh Ngan Dao <nguy2884@umn.edu>
2023-07-06 12:17:52 -0500

    started on app.js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
-
-import Header from '../Header/Header.jsx'
-import './App.css';
-
+import React, { useState } from "react";
+import Header from "../Header/Header.jsx";
+import "./App.css";
+import { useState, useEffect } from "react";
+import
 
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            <main>
-                <p>Under Construction...</p>
-            </main>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Header />
+      {/* title */}
+      {/* Form */}
+      {/* title */}
+      {/* button */}
+      {/* card list */}
+    </div>
+  );
 }
 
 export default App;
