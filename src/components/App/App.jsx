a6d13c311ad3ae5adc8bad25ebcc192950f4713a
Aubrey Graves <grav0165@gmail.com>
2023-07-06 13:56:02 -0500

    Built the framework of the DELETE request to the database through Axios currently in App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 import Header from '../Header/Header.jsx';
 import ShoppingButton from '../ShoppingButton/ShoppingButton.jsx'
@@ -6,6 +7,22 @@ import './App.css';
 
 
 function App() {
+
+    const deleteItem = (idToDelete) => {
+        axios({
+            method: 'DELETE',
+            url: `/item/`
+        })
+        .then( response => {
+            console.log('Deleted an item');
+            // getItems()
+        })
+        .catch( error => {
+            console.log('Error in DELETE request: ', error);
+            alert('Error in deleting an item');
+        })
+    }
+
     return (
         <div className="App">
             <Header />
