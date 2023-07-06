ea09be1f07714fe70fa24ac31c8883afe5de52b7
Jonathan Viegut <viegutj@gmail.com>
2023-07-06 13:40:44 -0500

    added imports for useState useEffect and Axios; started form

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,26 @@ import React from 'react';
 
 import Header from '../Header/Header.jsx'
 import './App.css';
+// import needed libraries for React routes
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
+
+let [newItem, setNewItem] = useState('');
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <form>
+                <label>
+                    Item
+                </label>
+                    <input type="text" 
+                    onChange={(event) => setNewItem(event.target.value)}
+                    value={newItem}
+                    />
+            </form>
             <main>
                 <p>Under Construction...</p>
             </main>
