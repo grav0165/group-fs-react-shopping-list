3a5fa03e37f99975fc5a1dc4604ad066e059a2fd
Aubrey Graves <grav0165@gmail.com>
2023-07-07 10:46:04 -0500

    Added confirmation for reset items buttons

--- src/components/ShoppingButton/ShoppingButton.jsx
+++ src/components/ShoppingButton/ShoppingButton.jsx
@@ -1,10 +1,26 @@
 import Button from "react-bootstrap/Button";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 function ShoppingButton({resetItems}) {
+  const MySwal = withReactContent(Swal);
+
   const handleReset = () => {
+    MySwal.fire({
+      title: "Do you want to remove all purchases?",
+      showDenyButton: true,
+      confirmButtonText: "Yes",
+      denyButtonText: "No",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // handleUPDATE
+        resetItems();
+        MySwal.fire("Success");
+      } else if (result.isDenied) {
+        MySwal.fire("Cancelled");
+      }
+    });
     console.log("Reset button clicked");
-    resetItems()
-    console.log('Items reset')
   };
 
   const handleClear = () => {
